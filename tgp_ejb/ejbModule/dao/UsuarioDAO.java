022df02729d8e51b1f874ebaa1aa0e2238ad0590
delessa <delessa@localhost>
2015-03-19 15:18:15 -0300

    find by

--- tgp_ejb/ejbModule/dao/UsuarioDAO.java
+++ tgp_ejb/ejbModule/dao/UsuarioDAO.java
@@ -1,5 +1,9 @@
 package dao;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.ejb.Stateless;
 
 import model.Usuario;
@@ -13,6 +17,16 @@ public class UsuarioDAO extends GenericDAO<Usuario> {
 		super(Usuario.class);
 	}
 	
+	public List<Usuario> listarPorLogin(String login){
+		Map<String, Object> parameters = new HashMap<String, Object>();
+	    parameters.put("login",login);     
+	    return super.findListResult(Usuario.FIND_BY_LOGIN,parameters);
+	}
 	
+	public List<Usuario> listarPoreEmail(String email){
+		Map<String, Object> parameters = new HashMap<String, Object>();
+	    parameters.put("email",email);     
+	    return super.findListResult(Usuario.FIND_BY_EMAIL,parameters);
+	}
 	 
 }
